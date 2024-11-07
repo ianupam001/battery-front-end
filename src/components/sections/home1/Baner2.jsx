7134d32306a8029fd617aa55dc64e4bc3baebfe5
vipul <vipul.devs99@gmail.com>
2024-11-07 16:33:46 +0530

    fix

--- src/components/sections/home1/Baner2.jsx
+++ src/components/sections/home1/Baner2.jsx
@@ -1,62 +1,26 @@
-import { Link } from "react-router-dom";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { ContactFormModal } from "../../elements/ContactFormModal";
-import { useEffect, useState } from "react";
-
-const swiperOptions = {
-  modules: [Autoplay, Pagination, Navigation],
-  slidesPerView: 1,
-  spaceBetween: 0,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  loop: true,
-
-  // Navigation
-  navigation: {
-    nextEl: ".h1n",
-    prevEl: ".h1p",
-  },
-
-  // Pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-};
 
 export default function Banner() {
-  const apiUrl = import.meta.env.VITE_BASE_URL;
-  const [sourcePage, setSourcePage] = useState("");
-
-  const [sliders, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch(`${apiUrl}/api/slider/getSliders?limit=1`);
-      const blogslider = await res.json();
-      setPosts(blogslider.sliders);
-    };
-    fetchPosts();
-  }, []);
 
   return (
     <>
+      {/* Mobile Banner */}
       <section className="md:hidden relative flex flex-col">
         <div>
           <img
             src="assets/images/banner-mobile.jpeg"
-            alt="Banner"
-            className="w-full "
+            srcSet="assets/images/banner-mobile-light.jpeg 480w"
+            sizes="(max-width: 600px) 480px, (max-width: 960px) 768px, 1200px"
+            alt="Mobile Banner"
+            className="w-full"
           />
         </div>
         <div className="-mt-10 flex justify-center mx-10">
-          <ContactFormModal sourcePage={sourcePage} />
+          <ContactFormModal sourcePage="mobile" />
         </div>
       </section>
 
+      {/* Desktop Banner */}
       <section className="hidden md:block">
         <div
           className="flex justify-between items-center"
@@ -72,7 +36,7 @@ export default function Banner() {
           <div className="container flex justify-between items-center banner_section">
             <div className="main-slider-three__content banner_content"></div>
             <div className="main-slider-three__content banner_form">
-              <ContactFormModal sourcePage={sourcePage} />
+              <ContactFormModal sourcePage="desktop" />
             </div>
           </div>
         </div>
